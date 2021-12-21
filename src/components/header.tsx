18edde29d117e0558ac260fea7faf873bbfc0984
MetaAngelDAO <96368688+MetaAngelDAO@users.noreply.github.com>
2021-12-21 09:55:18 +0300

    delete

--- src/components/header.tsx
+++
@@ -1,9 +0,0 @@
-import { VFC } from 'react';
-
-import s from './header.module.scss';
-
-const Header: VFC = () => {
-  return <header className={s.header}>META ANGEL DAO</header>;
-};
-
-export default Header;
